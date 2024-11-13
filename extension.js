32d3083bfb8a6f69687012f9f1407eaa591e982d
sgc93 <smachewgedefaw@gmail.com>
2024-11-13 06:46:51 -0900

    clear autogenerated command lines

--- extension.js
+++ extension.js
@@ -1,28 +1,21 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const vscode = require("vscode");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	console.log(
+		'Congratulations, your extension "my-first-extension" is now active!'
+	);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "my-first-extension" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('my-first-extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from My first extension!');
-	});
+	const disposable = vscode.commands.registerCommand(
+		"my-first-extension.helloWorld",
+		function () {
+			vscode.window.showInformationMessage(
+				"Hello World from My first extension!"
+			);
+		}
+	);
 
 	context.subscriptions.push(disposable);
 }
@@ -32,5 +25,5 @@ function deactivate() {}
 
 module.exports = {
 	activate,
-	deactivate
-}
+	deactivate,
+};
