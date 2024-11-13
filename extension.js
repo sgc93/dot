ca9333c1975620e8a8467958ae4abd9d5950053d
sgc93 <smachewgedefaw@gmail.com>
2024-11-13 11:52:11 -0900

    handle opening aproject in new file

--- extension.js
+++ extension.js
@@ -5,26 +5,6 @@ const axios = require("axios");
  * @param {vscode.ExtensionContext} context
  */
 
-async function searchProjects() {
-	try {
-		const res = await axios.get(`http://127.0.0.1:9000/api/v1/projects`);
-		const projectItems = res.data.data.docs.map((project) => ({
-			label: project.name,
-			description: project.description,
-			detail: `By: ${project.owner.name}, Stars: ${project.likes.length}, Comments: ${project.comments.length}`,
-			link: `http://localhost:5173/community/project/${project._id}`,
-		}));
-
-		return projectItems;
-	} catch (err) {
-		console.log("Error fetching projects from DotCode Database: ", err.message);
-		vscode.window.showErrorMessage(
-			"Failed to load projects from DotCode server😕"
-		);
-		return null;
-	}
-}
-
 function activate(context) {
 	console.log('Congratulations, your extension "MyVSC" is now active!');
 
@@ -44,10 +24,12 @@ function activate(context) {
 				if (selectedProject === undefined) {
 					vscode.window.showInformationMessage("No project is selected👍🏻");
 				} else {
+					await openProjectContent(selectedProject);
 					vscode.window.showInformationMessage(
 						`You have selected ${selectedProject.label}`
 					);
-					vscode.env.openExternal(selectedProject.link);
+					// opening the selected project in the dotcode website
+					// vscode.env.openExternal(selectedProject.link);
 				}
 
 				console.log(selectedProject);
@@ -56,6 +38,52 @@ function activate(context) {
 	);
 
 	context.subscriptions.push(disposable);
+}
+
+async function searchProjects() {
+	try {
+		const res = await axios.get(`http://127.0.0.1:9000/api/v1/projects`);
+		const projectItems = res.data.data.docs.map((project) => ({
+			_id: project._id,
+			lngName: project.lngName === "react" ? "javascript" : project.lngName,
+			description: project.description,
+			type: project.type,
+			code: project.code,
+			label: project.name,
+			detail: `By: ${project.owner.name} | Stars: ${
+				project.likes.length
+			} | Comments: ${project.comments.length} | ${
+				project.type === "ui"
+					? "UI with HTML, CSS and JS"
+					: project.lngName === "react"
+					? "UI with REACT"
+					: "Snippet with " + project.lngName
+			} `,
+			link: `http://localhost:5173/community/project/${project._id}`,
+		}));
+
+		return projectItems;
+	} catch (err) {
+		console.log("Error fetching projects from DotCode Database: ", err.message);
+		vscode.window.showErrorMessage(
+			"Failed to load projects from DotCode server😕"
+		);
+		return null;
+	}
+}
+
+async function openProjectContent(project) {
+	const isSnippet = project.type === "snippet";
+	try {
+		const document = await vscode.workspace.openTextDocument({
+			content: isSnippet ? project.code.code : project.code.html,
+			language: isSnippet ? project.lngName : "html",
+		});
+
+		await vscode.window.showTextDocument(document, { preview: false });
+	} catch (error) {
+		vscode.window.showErrorMessage(`Failed to open project: ${error.message}`);
+	}
 }
 
 function deactivate() {}
