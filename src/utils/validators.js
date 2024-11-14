e96872cd4aac72d6e3bddbe293b735c9117c9b04
sgc93 <smachewgedefaw@gmail.com>
2024-11-13 23:15:44 -0900

    no need to add a 'export' tag : forgetting the fact that i am working with NODE😁

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,8 +1,8 @@
-export const isCurrLngModeSupported = (currLngMode) => {
+const isCurrLngModeSupported = (currLngMode) => {
 	// check if the current lng mode is supported
 };
 
-export const isCurrLngModeUiLng = (currLngMode) => {
+const isCurrLngModeUiLng = (currLngMode) => {
 	if (currLngMode === "css" || currLngMode === "html" || currLngMode === "js") {
 		return true;
 	} else {
