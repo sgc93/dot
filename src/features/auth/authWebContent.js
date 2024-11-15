e9d440d12f3394830d9ea04b29b0d6a1fe7a2905
sgc93 <smachewgedefaw@gmail.com>
2024-11-15 08:06:07 -0900

    handle pre load togglig between signup and login form based on the command

--- src/features/auth/authWebContent.js
+++ src/features/auth/authWebContent.js
@@ -7,8 +7,21 @@ function getAuthWebContent(action) {
 
 	const htmlContent = fs.readFileSync(htmlPath, "utf8");
 	const cssContent = fs.readFileSync(cssPath, "utf8");
-
-	return htmlContent.replace("{{style}}", `<style>${cssContent}</style>`);
+	if (action === "Sign Up") {
+		const updatedHtml = htmlContent
+			.replace("{{loginBtnInitClasses}}", "header-btn")
+			.replace("{{signUpBtnInitClasses}}", "header-btn active")
+			.replace("{{loginFormInitClasses}}", "hidden")
+			.replace("{{signUpFormInitClasses}}", "shown");
+		return updatedHtml.replace("{{style}}", `<style>${cssContent}</style>`);
+	} else {
+		const updatedHtml = htmlContent
+			.replace("{{loginBtnInitClasses}}", "header-btn active")
+			.replace("{{signUpBtnInitClasses}}", "header-btn")
+			.replace("{{loginFormInitClasses}}", "shown")
+			.replace("{{signUpFormInitClasses}}", "hidden");
+		return updatedHtml.replace("{{style}}", `<style>${cssContent}</style>`);
+	}
 }
 
 module.exports = getAuthWebContent;
