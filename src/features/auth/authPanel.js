e427f27514fbda0dc9f8b77b34e0c7c438e65e6c
sgc93 <smachewgedefaw@gmail.com>
2024-11-15 12:09:38 -0900

    handle recieving data from auth webview

--- src/features/auth/authPanel.js
+++ src/features/auth/authPanel.js
@@ -1,7 +1,7 @@
 const getAuthWebContent = require("./authWebContent");
 const vscode = require("vscode");
 
-const dotCodeAuthPanel = (action) => {
+const dotCodeAuthPanel = (action, context) => {
 	const panel = vscode.window.createWebviewPanel(
 		"account",
 		`DotCode - ${action}`,
@@ -12,6 +12,22 @@ const dotCodeAuthPanel = (action) => {
 	);
 
 	panel.webview.html = getAuthWebContent(action);
+	panel.webview.onDidReceiveMessage(
+		(message) => {
+			const data = message.data;
+			if (message.command === "login") {
+				vscode.window.showInformationMessage(
+					`Login data:- email: ${data.email}, password: ${data.password}`
+				);
+			} else if (message.command === "signUp") {
+				vscode.window.showInformationMessage(
+					`Sign Up data:- name: ${data.name}, email: ${data.email}, password: ${data.password}, confirmPassword: ${data.confirmPassword}`
+				);
+			}
+		},
+		undefined,
+		context.subscriptions
+	);
 };
 
 module.exports = dotCodeAuthPanel;
