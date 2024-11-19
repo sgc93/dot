4cf6a304db837ff1c32f7bbc41d5eeaa9c735ef4
sgc93 <smachewgedefaw@gmail.com>
2024-11-18 22:25:57 -0900

    handle messages received from webveiw content of sidebar view

--- src/features/sidebar/dotCodeWebiewProvieder.js
+++ src/features/sidebar/dotCodeWebiewProvieder.js
@@ -1,4 +1,36 @@
 const sideViewContent = require("./htmlContent");
+const userData = require("../../utils/userData");
+const vscode = require("vscode");
+
+const handleReceivedMessage = (message) => {
+	switch (message.command) {
+		case "redirect":
+			vscode.env.openExternal(message.data);
+			break;
+		case "account":
+			vscode.commands.executeCommand("my-first-extension.myAccount");
+			break;
+		case "projects":
+			vscode.commands.executeCommand("my-first-extension.myProjects");
+			break;
+		case "search":
+			vscode.commands.executeCommand(
+				"my-first-extension.searchDotCodeProjects"
+			);
+			break;
+		case "signup":
+			vscode.commands.executeCommand("my-first-extension.signUp");
+			break;
+		case "login":
+			vscode.commands.executeCommand("my-first-extension.login");
+			break;
+		case "create":
+			vscode.window.showInformationMessage(
+				"select a code if it exists or write it on a file then select it, a popup info box will tell you to save it automatically"
+			);
+			break;
+	}
+};
 
 class DotCodeWebViewProvider {
 	constructor(context) {
@@ -10,11 +42,13 @@ class DotCodeWebViewProvider {
 			enableScripts: true,
 		};
 
-		webviewView.webview.html = sideViewContent();
+		const user = userData.getUserData(this.context);
 
-		webviewView.webview.onDidReceiveMessage(() => {
-			// message will be handled here
-		});
+		webviewView.webview.html = sideViewContent(user && user.token, user);
+
+		webviewView.webview.onDidReceiveMessage((message) =>
+			handleReceivedMessage(message)
+		);
 	}
 }
 
