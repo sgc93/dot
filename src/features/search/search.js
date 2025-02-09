b9e65e0dd27fab10abaa627524c39b0e34198db7
sgc93 <smachewgedefaw@gmail.com>
2025-02-09 01:43:58 -0900

    udpate url

--- src/features/search/search.js
+++ src/features/search/search.js
@@ -20,58 +20,56 @@ async function search() {
 		return;
 	} else {
 		const searchResults = await searchProjects(query);
-		if (searchResults.results > 0) {
-			const selectedProject = await vscode.window.showQuickPick(
-				searchResults.projectItems,
-				{
-					title: "DotCode Project Search Results",
-					placeHolder: "Search Projects here ..",
-				}
-			);
+    if (searchResults.results > 0) {
+      const selectedProject = await vscode.window.showQuickPick(
+        searchResults.projectItems,
+        {
+          title: "DotCode Project Search Results",
+          placeHolder: "Search Projects here .."
+        }
+      );
 
-			if (selectedProject === undefined) {
-				vscode.window.showInformationMessage("No project is selected👍🏻");
-			} else {
-				const selectedAction = await vscode.window.showQuickPick(
-					constants.searchResultActions,
-					{
-						title: `What to do with ${selectedProject.label}?`,
-						placeHolder: "Search Action here ..",
-					}
-				);
+      if (selectedProject === undefined) {
+        vscode.window.showInformationMessage("No project is selected👍🏻");
+      } else {
+        const selectedAction = await vscode.window.showQuickPick(
+          constants.searchResultActions,
+          {
+            title: `What to do with ${selectedProject.label}?`,
+            placeHolder: "Search Action here .."
+          }
+        );
 
-				if (selectedAction === undefined) {
-					vscode.window.showInformationMessage(
-						`No Action is selected for '${selectedProject.label}'.`
-					);
-				} else {
-					if (selectedAction.action === "insert") {
-						handleResults.insertContentAtCursor(selectedProject);
-					} else if (selectedAction.action === "open") {
-						handleResults.openProjectContent(selectedProject);
-					} else if (selectedAction.action === "redirect") {
-						handleResults.openInDotCode(selectedProject);
-					}
-				}
-			}
-		} else {
-			vscode.window
-				.showErrorMessage(
-					`No result found for ${query}: try again with project name, description ro tags`,
-					"Retry",
-					"Visit DotCode Web"
-				)
-				.then((selection) => {
-					if (selection === "Retry") {
-						vscode.commands.executeCommand("dot-code.searchDotCodeProjects");
-					} else if (selection === "Visit DotCode Web") {
-						vscode.env.openExternal(
-              "https://unlikely-brina-sgc-6053c46d.koyeb.app/community"
-            );
-					}
-				});
-			return;
-		}
+        if (selectedAction === undefined) {
+          vscode.window.showInformationMessage(
+            `No Action is selected for '${selectedProject.label}'.`
+          );
+        } else {
+          if (selectedAction.action === "insert") {
+            handleResults.insertContentAtCursor(selectedProject);
+          } else if (selectedAction.action === "open") {
+            handleResults.openProjectContent(selectedProject);
+          } else if (selectedAction.action === "redirect") {
+            handleResults.openInDotCode(selectedProject);
+          }
+        }
+      }
+    } else {
+      vscode.window
+        .showErrorMessage(
+          `No result found for ${query}: try again with project name, description ro tags`,
+          "Retry",
+          "Visit DotCode Web"
+        )
+        .then((selection) => {
+          if (selection === "Retry") {
+            vscode.commands.executeCommand("dot-code.searchDotCodeProjects");
+          } else if (selection === "Visit DotCode Web") {
+            vscode.env.openExternal("http://localhost:5173/community");
+          }
+        });
+      return;
+    }
 	}
 }
 
