ccc140076d9af632a1bfca097a0b43f0acdf2f57
sgc93 <smachewgedefaw@gmail.com>
2025-02-09 01:41:45 -0900

    create validator method to check code content

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -2,12 +2,30 @@ const isCurrLngModeSupported = (currLngMode) => {
 	// check if the current lng mode is supported
 };
 
-const isCurrLngModeUiLng = (currLngMode) => {
-	if (currLngMode === "css" || currLngMode === "html" || currLngMode === "js") {
-		return true;
-	} else {
-		return false;
-	}
+const isCodeEmpty = (project) => {
+  if (project.type === "ui") {
+    if (
+      project.code[0].html.trim().length === 0 &&
+      project.code[0].css.trim().length === 0 &&
+      project.code[0].js.trim().length === 0
+    ) {
+      return true;
+    }
+  } else {
+    if (project.code[0].code.trim().length === 0) {
+      return true;
+    }
+  }
+
+  return false;
 };
 
-module.exports = { isCurrLngModeSupported, isCurrLngModeUiLng };
+const isCurrLngModeUiLng = (currLngMode) => {
+  if (currLngMode === "css" || currLngMode === "html" || currLngMode === "js") {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+module.exports = { isCurrLngModeSupported, isCurrLngModeUiLng, isCodeEmpty };
